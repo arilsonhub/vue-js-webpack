e4eca23d59ae77c078285b2a6a329cc1aecb4288
Arilson <arilson.senac.ads@gmail.com>
2019-10-28 18:03:11 -0300

    Cors Config

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use('/static', express.static(path.join(__dirname,"/dist/static/"))); 
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
+
 app.get('/', function(req,res) {
    res.sendFile('index.html', { root: path.join(__dirname, '/dist/') });
 });
